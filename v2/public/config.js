efc93cdb1c245a23d16772af7ad7c2f8b82ebd3f
Alexandre Miranda <165743501+Aleraca017@users.noreply.github.com>
2025-04-14 21:38:18 -0300

    redirect pages

---
+++ v2/public/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAznTyn2JKhPuM2QOJH91r_k3pl89WtiV8",
+      authDomain: "dmevolution-7639a.firebaseapp.com",
+      projectId: "dmevolution-7639a",
+      storageBucket: "dmevolution-7639a.appspot.com",
+      messagingSenderId: "1060243812509",
+      appId: "1:1060243812509:web:f58d7a5f7c52562e946474"
+}
